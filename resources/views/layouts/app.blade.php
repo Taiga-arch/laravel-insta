07341431fa8c94fc8e91411d2280d67c21ec7be7
坂本泰雅 <sakamotoyasushimiyabi@sakamotoyasushimiyabis-MacBook-Air.local>
2025-07-24 21:22:43 +0900

    message here

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    <!-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) -->
+   {{-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) --}}
 
     <script src="https://kit.fontawesome.com/dbc5b98639.js" crossorigin="anonymous"></script>
 
